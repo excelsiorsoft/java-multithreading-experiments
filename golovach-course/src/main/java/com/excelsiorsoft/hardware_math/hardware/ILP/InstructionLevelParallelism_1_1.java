e4abfaf1bfe345bae40933893bdaac9748729325
sleyzerzon <senya22@yahoo.com>
2016-09-01 15:31:34 -0400

    ten different numbers

--- golovach-course/src/main/java/com/excelsiorsoft/hardware_math/hardware/ILP/InstructionLevelParallelism_1_1.java
+++ golovach-course/src/main/java/com/excelsiorsoft/hardware_math/hardware/ILP/InstructionLevelParallelism_1_1.java
@@ -44,6 +44,64 @@ public class InstructionLevelParallelism_1_1 {
 		System.out.println("result: " +d0);
 		return took;
 	}
+	
+	public long squaringSeparateTwoNums() {
+		long t0 = System.currentTimeMillis();
+		double d0 = 0;
+		double d1 = 0;
+		for (int k = 0; k < 100_000_000; k++){
+			
+			//check bytecode for below to see why timing differs
+			d0 *= d0;  			//faster
+			d1 *= d1;  			//faster
+			//d0 *= d0 * d0;  	//slower
+		}
+		
+		long t1 = System.currentTimeMillis();
+		long took = (t1 - t0);
+		System.out.println("took: "+took + " ms");
+		System.out.println("result: " +d0);
+		return took;
+	}
+	
+	public long squaringSeparateTenNums() {
+		long t0 = System.currentTimeMillis();
+		
+		double d0 = 0;
+		double d1 = 0;
+		double d2 = 0;
+		double d3 = 0;
+		double d4 = 0;
+		double d5 = 0;
+		double d6 = 0;
+		double d7 = 0;
+		double d8 = 0;
+		double d9 = 0;
+		double d10 = 0;
+		
+		for (int k = 0; k < 100_000_000; k++){
+			
+			//check bytecode for below to see why timing differs
+			d0 *= d0;  			//faster
+			d1 *= d1;  			//faster
+			d2 *= d2;  			//faster
+			d3 *= d3;  			//faster
+			d4 *= d4;  			//faster
+			d5 *= d5;  			//faster
+			d6 *= d6;  			//faster
+			d7 *= d7;  			//faster
+			d8 *= d8;  			//faster
+			d9 *= d9;  			//faster
+			d10 *= d10;  			//faster
+			//d0 *= d0 * d0;  	//slower
+		}
+		
+		long t1 = System.currentTimeMillis();
+		long took = (t1 - t0);
+		System.out.println("took: "+took + " ms");
+		System.out.println("result: " +d0);
+		return took;
+	}
 
 	
 }
--- golovach-course/src/test/java/com/excelsiorsoft/hardware_math/hardware/ILP/InstructionLevelParallelism_1_1Test.java
+++ golovach-course/src/test/java/com/excelsiorsoft/hardware_math/hardware/ILP/InstructionLevelParallelism_1_1Test.java
@@ -33,5 +33,31 @@ public class InstructionLevelParallelism_1_1Test {
 		System.out.println("average: "+avg);
 		
 	}
+	
+	@Test
+	public void testSquaringTwoSeparateNums() {
+		int repetitions = 10;
+		long sum = 0;
+		for (int i = 0; i < repetitions; i++) {
+			sum += cut.squaringSeparateTwoNums();
+			System.out.println("accumulated: "+ sum + "\n-------------------");
+		}
+		double avg = sum/repetitions;
+		System.out.println("average: "+avg);
+		
+	}
+	
+	@Test
+	public void testSquaringTenSeparateNums() {
+		int repetitions = 10;
+		long sum = 0;
+		for (int i = 0; i < repetitions; i++) {
+			sum += cut.squaringSeparateTenNums();
+			System.out.println("accumulated: "+ sum + "\n-------------------");
+		}
+		double avg = sum/repetitions;
+		System.out.println("average: "+avg);
+		
+	}
 
 }
