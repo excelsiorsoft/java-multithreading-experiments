c728dd76fc148cf2ead93efe1596ccd0b6d2f914
sleyzerzon <senya22@yahoo.com>
2016-09-05 17:50:07 -0400

    producer consumer

---
+++ golovach-course/src/main/java/com/excelsiorsoft/java_util_concurrent/ProducerConsumer.java
@@ -0,0 +1,53 @@
+package com.excelsiorsoft.java_util_concurrent;
+
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+
+public class ProducerConsumer {
+
+	public static void main(String... strings) {
+		
+		final BlockingQueue<Integer> queue = new ArrayBlockingQueue<>(16);
+		
+		//Producer
+		new Thread(new Runnable() {
+			public void run() {
+				int counter = 0;
+
+				while (true) {
+					
+					try {
+						Thread.sleep(300);
+						queue.put(++counter);
+						System.out.println("put: "+counter);
+					} catch (InterruptedException ignore) {	}
+				}
+			}
+		}).start();
+		
+		
+		//Consumer
+		new Thread(new Runnable() {
+			public void run() {
+				
+				int data = 0;
+				while (true) {
+					
+					try {
+						data = queue.take();
+						System.out.println("took: "+ data);
+					} catch (InterruptedException ignore) {	}
+						process(data);
+				}
+			}
+
+			private void process(int data) {
+				
+				System.out.println("Process: "+ data+"\n=================");
+			}
+		}).start();
+	}
+	
+	
+	
+}
