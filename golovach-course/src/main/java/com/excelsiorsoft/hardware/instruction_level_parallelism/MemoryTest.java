eb722ad7133002a29314cf985196627ba744e173
sleyzerzon <senya22@yahoo.com>
2016-09-06 13:25:56 -0400

    checking memory

---
+++ golovach-course/src/main/java/com/excelsiorsoft/hardware/instruction_level_parallelism/MemoryTest.java
@@ -0,0 +1,28 @@
+package com.excelsiorsoft.hardware.instruction_level_parallelism;
+
+/**
+ * Run from the console: 
+
+    $ java -Xms1025k -Xmx1025k -XshowSettings:vm  MemoryTest
+
+
+ * @author Simeon
+ *
+ */
+
+public class MemoryTest {
+	public static void main(String args[]) {
+		System.out.println("Used Memory   :  "
+				+ (Runtime.getRuntime().totalMemory() - Runtime.getRuntime()
+						.freeMemory()) + " bytes");
+		
+		System.out.println("Free Memory   : "
+				+ Runtime.getRuntime().freeMemory() + " bytes");
+		
+		System.out.println("Total Memory  : "
+				+ Runtime.getRuntime().totalMemory() + " bytes");
+		
+		System.out.println("Max Memory    : "
+				+ Runtime.getRuntime().maxMemory() + " bytes");
+	}
+}
