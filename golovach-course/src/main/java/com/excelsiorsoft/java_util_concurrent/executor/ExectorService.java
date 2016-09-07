22f53cfd7ea5906bd4ae90d2308d031c0a83c514
sleyzerzon <senya22@yahoo.com>
2016-09-07 16:24:23 -0400

    calling get()

--- golovach-course/src/main/java/com/excelsiorsoft/java_util_concurrent/executor/ExectorService.java
+++ golovach-course/src/main/java/com/excelsiorsoft/java_util_concurrent/executor/ExectorService.java
@@ -7,25 +7,37 @@ import java.util.concurrent.Future;
 public class ExectorService {
 
 	public static void main(String[] args) throws Exception {
-	ExecutorService execService = java.util.concurrent.Executors.newCachedThreadPool();
-	
-	Future<Integer> future0 = execService.submit(new Callable<Integer>() {
-		public Integer call() {
-			return 42;
+		ExecutorService execService = java.util.concurrent.Executors
+				.newCachedThreadPool();
+
+		Future<Integer> future0 = execService.submit(new Callable<Integer>() {
+			public Integer call() {
+				return 42;
+			}
 		}
-	});
-	
-	Future<Integer> future1 = execService.submit(new Callable<Integer>() {
-		public Integer call()  {
-			return null;
-			//while(true);
-		}
-	});
-	
-	Thread.sleep(1000);
-	
-	System.out.println("future0.isDone: "+future0.isDone());
-	System.out.println("future1.isDone: "+future1.isDone());//returns true for `null` and false for the `while(true)` loop
-	
+
+		);
+
+		Future<Integer> future1 = execService.submit(new Callable<Integer>() {
+			public Integer call() {
+				return null;
+				// while(true);
+			}
+		});
+
+		Thread.sleep(1000);
+
+		System.out.println("future0.isDone: " + future0.isDone());
+		System.out.println("future1.isDone: " + future1.isDone());// returns
+																	// true for
+																	// `null`
+																	// and false
+																	// for the
+																	// `while(true)`
+																	// loop
+		
+		System.out.println("future0.get(): "+future0.get());
+		System.out.println("future1.get(): "+future1.get()); //blocks for `while(true)`
+
 	}
 }
