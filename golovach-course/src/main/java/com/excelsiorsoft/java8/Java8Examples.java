59b18c035d7f9509a79949539f93949af061b602
sleyzerzon <senya22@yahoo.com>
2016-09-07 20:41:40 -0400

    filters

--- golovach-course/src/main/java/com/excelsiorsoft/java8/Java8Examples.java
+++ golovach-course/src/main/java/com/excelsiorsoft/java8/Java8Examples.java
@@ -1,6 +1,7 @@
 package com.excelsiorsoft.java8;
 
 import java.util.Arrays;
+import java.util.stream.Stream;
 
 public class Java8Examples {
 	
@@ -10,7 +11,18 @@ public class Java8Examples {
 		
 		new Thread(()->System.out.println("Lambda!")).start();
 		
-		Arrays.asList("A", "BB", "CCC").parallelStream().map(/*str -> str.length()*/String::length).filter(k->k%2 == 1).forEach(System.out::println);
+		Arrays.asList("A", "BB", "CCC").parallelStream()
+										.map(/*str -> str.length()*/String::length)
+										.filter(k->k%2 == 1)
+										.forEach(System.out::println);
+		
+		System.out.println("=========================");
+		
+		 Stream.iterate(2L, k->k+3).limit(20).forEach(System.out::println);;
+		 
+		 System.out.println("=========================");
+			
+		 Stream.iterate(2L, k->k+1).filter(k -> k%3==2).limit(20).forEach(System.out::println);;
 	}
 
 	
