40954b2e60bd3722e7cd3a1c16c2f7b8aa28e160
sleyzerzon <senya22@yahoo.com>
2016-09-09 07:34:48 -0400

    polishing tests

--- golovach-course/src/main/java/com/excelsiorsoft/java_util_concurrent/synchronization/Synchronization_LockingStrategy.java
+++ golovach-course/src/main/java/com/excelsiorsoft/java_util_concurrent/synchronization/Synchronization_LockingStrategy.java
@@ -17,27 +17,57 @@ public class Synchronization_LockingStrategy {
 	
 	public static void main(String []args) throws InterruptedException {
 		
+		int toInc = 10;
+		int noOfThreads = 2;
+		
 		int repetitions = 10_000;
 		long accumulatedTime = 0;
 		
 		for(int i=0; i<repetitions;i++) {
-		long took = new Synchronization_LockingStrategy().withSynchronized();
+		long took = new Synchronization_LockingStrategy().withSynchronized(toInc, noOfThreads);
 		accumulatedTime += took;
 		}
 		
 		long avgTime = accumulatedTime/repetitions;
 		
-		System.out.println("========================\nAverage Time: "+(double)avgTime / 1E9+" sec");
+		System.out.println("Finished "+repetitions+ " repetitions of incrementing "+toInc+" numbers with " + noOfThreads + " threads in " + accumulatedTime / 1E9 +" sec");
+		
+		System.out.println("========================\nAverage Time per repetition: "+(double)avgTime / 1E9+" sec");
 		//System.out.println("========================\nAverage Time: "+avgTime);
 		
 	}
 	
-	public long withSynchronized() throws InterruptedException {
+	public long withSynchronized(int toInc, int noOfThreads) throws InterruptedException {
 		
 		long startTime = System.nanoTime();
 		
-		Thread t1 = new Thread(() -> {
-			for(int i=0; i<10;i++) {
+Thread[] threadsCreated = new Thread[noOfThreads];
+		
+		for(int th=0; th<noOfThreads; th++) {
+			
+			Runnable task = () -> {
+				for(int i=0; i<toInc;i++) {
+					synchronized (mutex) {
+						int myId = index++;
+						System.out.println(Thread.currentThread()+": " + myId);
+					}
+				}
+			};
+			
+			Thread t = new Thread(task);
+			t.setName("t"+th);
+			threadsCreated[th] = t;
+			//t.setPriority(Thread.NORM_PRIORITY - 1);
+			t.start();
+		}
+		
+		
+		for(Thread th: threadsCreated) {
+			th.join();
+		}
+		
+		/*Thread t1 = new Thread(() -> {
+			for(int i=0; i<toInc;i++) {
 				synchronized (mutex) {
 					int myId = index++;
 					System.out.println(Thread.currentThread()+": " + myId);
@@ -53,7 +83,7 @@ public class Synchronization_LockingStrategy {
 		
 		
 		Thread t2 = new Thread(() -> {
-			for(int i=0; i<10;i++) {
+			for(int i=0; i<toInc;i++) {
 				synchronized (mutex) {
 					int myId = index++;
 					System.out.println(Thread.currentThread()+": " + myId);
@@ -68,7 +98,7 @@ public class Synchronization_LockingStrategy {
 		t2.start();
 		
 		t1.join();
-		t2.join();
+		t2.join();*/
 		
 		long took = (System.nanoTime() - startTime);
 		System.out.println("----------------------------\nestimatedTime: " +took+"\n----------------------------");
--- golovach-course/src/main/java/com/excelsiorsoft/java_util_concurrent/synchronization/WaitFree_LockingStrategy.java
+++ golovach-course/src/main/java/com/excelsiorsoft/java_util_concurrent/synchronization/WaitFree_LockingStrategy.java
@@ -5,6 +5,9 @@ package com.excelsiorsoft.java_util_concurrent.synchronization;
 
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Supplier;
 
 /**
  * @author Simeon
@@ -17,33 +20,70 @@ public class WaitFree_LockingStrategy {
 
 	
 	public static void main(String []args) throws InterruptedException {
-		/*new WaitFree_LockingStrategy().withTryLock();*/
-		
+			
+		int toInc = 10;
+		int noOfThreads = 2;
 		
 		int repetitions = 10_000;
 		long accumulatedTime = 0;
 		
 		for(int i=0; i<repetitions;i++) {
-		long took = new WaitFree_LockingStrategy().withTryLock();
+		long took = new WaitFree_LockingStrategy().withTryLock(toInc, noOfThreads);
 		accumulatedTime += took;
 		}
 		
 		long avgTime = accumulatedTime/repetitions;
+		System.out.println("Finished "+repetitions+ " repetitions of incrementing "+toInc+" numbers with " + noOfThreads + " threads in " + accumulatedTime / 1E9 +" sec");
 		
-		System.out.println("========================\nAverage Time: "+(double)avgTime / 1E9+" sec");
-		//System.out.println("========================\nAverage Time: "+avgTime);
+		System.out.println("========================\nAverage Time per repetition: "+(double)avgTime / 1E9+" sec");
+		
 		
 	}
 	
-	public long withTryLock() throws InterruptedException {
+	public long withTryLock(int toInc, int noOfThreads) throws InterruptedException {
 		
 		long startTime = System.nanoTime();
 		
-		Thread t1 = new Thread(() -> {
-			for(int i=0; i<10;i++) {
+		Thread[] threadsCreated = new Thread[noOfThreads];
+		
+		for(int th=0; th<noOfThreads; th++) {
+			
+			Runnable task = () -> {
+				for(int i=0; i<toInc;i++) {
+					label:
+					if (lock.tryLock()) {
+						try{ int myId = index++;
+						System.out.println(Thread.currentThread()+": " + myId);
+						break label;
+						}finally{lock.unlock();}
+					} else {
+						
+						System.out.println(Thread.currentThread()+" couldn't acquire lock. " );
+					}
+
+				}
+
+			};
+			
+			Thread t = new Thread(task);
+			t.setName("t"+th);
+			threadsCreated[th] = t;
+			//t.setPriority(Thread.NORM_PRIORITY - 1);
+			t.start();
+		}
+		
+		
+		for(Thread th: threadsCreated) {
+			th.join();
+		}
+		
+/*		Thread t1 = new Thread(() -> {
+			for(int i=0; i<toInc;i++) {
+				label:
 				if (lock.tryLock()) {
 					try{ int myId = index++;
 					System.out.println(Thread.currentThread()+": " + myId);
+					break label;
 					}finally{lock.unlock();}
 				} else {
 					
@@ -54,15 +94,17 @@ public class WaitFree_LockingStrategy {
 
 		}); 
 		t1.setName("t1");
-		t1.setPriority(Thread.NORM_PRIORITY - 1);
+		//t1.setPriority(Thread.NORM_PRIORITY - 1);
 		t1.start();;
 		
 		
 		Thread t2 = new Thread(() -> {
-			for(int i=0; i<10;i++) {
+			for(int i=0; i<toInc;i++) {
+				label:
 				if (lock.tryLock()) {
 					try{int myId = index++;
 					System.out.println(Thread.currentThread()+": " + myId);
+					break label;
 					}finally{lock.unlock();}
 				} else {
 					System.out.println(Thread.currentThread()+" couldn't acquire lock. " );
@@ -72,14 +114,14 @@ public class WaitFree_LockingStrategy {
 
 		}); 
 		t2.setName("t2");
-		t2.setPriority(Thread.NORM_PRIORITY );
+		//t2.setPriority(Thread.NORM_PRIORITY );
 		t2.start();
 		
 		t1.join();
-		t2.join();
+		t2.join();*/
 		
 		long took = (System.nanoTime() - startTime);
-		System.out.println("----------------------------\nestimatedTime: " +took+"\n----------------------------");
+		System.out.println("----------------------------\nestimatedTime: " +took/1E9+" sec\n----------------------------");
 		return took;
 	}
 	
