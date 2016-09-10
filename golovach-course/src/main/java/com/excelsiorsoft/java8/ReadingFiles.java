145807e29333e3dcbd63d2b022f7f3aa417a5543
sleyzerzon <senya22@yahoo.com>
2016-09-10 11:56:16 -0400

    invokeAll/Any

--- golovach-course/src/main/java/com/excelsiorsoft/java8/ReadingFiles.java
+++ golovach-course/src/main/java/com/excelsiorsoft/java8/ReadingFiles.java
@@ -1,5 +1,7 @@
 package com.excelsiorsoft.java8;
 
+import static java.nio.file.Files.readAllBytes;
+import static java.nio.file.Paths.get;
 import static java.util.Arrays.asList;
 
 import java.io.IOException;
@@ -7,6 +9,7 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -14,13 +17,17 @@ import java.util.concurrent.Future;
 
 public class ReadingFiles {
 
-	ExecutorService pool = Executors.newCachedThreadPool();
+	static ExecutorService pool = Executors.newCachedThreadPool();
 	
 	public static void main(String[]args) throws IOException, InterruptedException, ExecutionException {
 		ReadingFiles readingFiles = new ReadingFiles();
 		readingFiles.execute();
-		System.out.println("-------------------------");
+		System.out.println("----------invokeAll()---------------");
 		readingFiles.invokeAll();
+		System.out.println("----------invokeAny()---------------");
+		readingFiles.invokeAny();
+		
+		pool.shutdown();
 	}
 	
 	public void execute() throws InterruptedException, ExecutionException {
@@ -49,6 +56,24 @@ public class ReadingFiles {
 		System.out.println(listFutBytes.get(0).isDone());
 		System.out.println(listFutBytes.get(1).isDone());
 		System.out.println(listFutBytes.get(2).isDone());
+		
+		
+	
+	}
+	
+public void invokeAny() throws InterruptedException, ExecutionException {
+		
+		byte[] bytes =  pool.invokeAny(
+				asList(
+						(Callable<byte[]>)() -> readAllBytes(get("E:\\source\\java-multithreading-experiments\\golovach-course\\src\\test\\resources\\tmp0.txt")),
+						(Callable<byte[]>)() -> readAllBytes(get("E:\\source\\java-multithreading-experiments\\golovach-course\\src\\test\\resources\\tmp0.txt")),
+						(Callable<byte[]>)() -> readAllBytes(get("E:\\source\\java-multithreading-experiments\\golovach-course\\src\\test\\resources\\tmp0.txt"))
+					)
+				);
+		
+		
+		System.out.println(Arrays.toString(bytes));
+		
 		
 		
 	
