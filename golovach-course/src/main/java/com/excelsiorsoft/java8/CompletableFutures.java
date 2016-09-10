4e50c2c50c8c105024161ac75eff75d9e2195e9b
sleyzerzon <senya22@yahoo.com>
2016-09-10 12:42:29 -0400

    what am I doing?

--- golovach-course/src/main/java/com/excelsiorsoft/java8/CompletableFutures.java
+++ golovach-course/src/main/java/com/excelsiorsoft/java8/CompletableFutures.java
@@ -1,14 +1,38 @@
 package com.excelsiorsoft.java8;
 
+import static java.util.concurrent.CompletableFuture.supplyAsync;
+
 import java.util.concurrent.CompletableFuture;
 
 public class CompletableFutures {
 
 	public static void main(String[] args) {
 
+		CompletableFutures completableFutures = new CompletableFutures();
+		completableFutures.buildCommunism();
+		completableFutures.buildCommunismShortened();
+	}
+
+	private void buildCommunismShortened() {
+
+		supplyAsync(() -> "42")
+				.thenApply(Integer::valueOf)
+				.thenApply(x -> Math.PI * x * x)
+				.thenAccept(System.out::println);
+		
+		System.out.println("Still Death");
+
+	}
+
+	private  void buildCommunism() {
 		CompletableFuture<String> communism = CompletableFuture.supplyAsync(() -> {
-			for (int k = 0; k < Long.valueOf(1_000_000_000_000L); k++)
+			for (int k = 0; k < Long.MAX_VALUE; k++)
 				;
+			/*try {
+				Thread.sleep(1000000);
+			} catch (Exception e) {
+				
+			}*/
 			return "42";
 		});
 
