97d3355c2966a584add44dadad5d07cdc9a0e8e1
sleyzerzon <senya22@yahoo.com>
2016-09-10 12:30:48 -0400

    communism allegory

--- golovach-course/src/main/java/com/excelsiorsoft/java8/CompletableFutures.java
+++ golovach-course/src/main/java/com/excelsiorsoft/java8/CompletableFutures.java
@@ -6,15 +6,15 @@ public class CompletableFutures {
 
 	public static void main(String[] args) {
 
-		CompletableFuture<String> f0 = CompletableFuture.supplyAsync(() -> {
-			for (int k = 0; k < Long.valueOf(1_000_000L); k++)
+		CompletableFuture<String> communism = CompletableFuture.supplyAsync(() -> {
+			for (int k = 0; k < Long.valueOf(1_000_000_000_000L); k++)
 				;
 			return "42";
 		});
 
-		CompletableFuture<Integer> f1 = f0.thenApply(Integer::valueOf);
-		CompletableFuture<Double> f2 = f1.thenApply( x -> Math.PI*x*x);
-		f2.thenAccept(System.out::println);
+		CompletableFuture<Integer> fridge = communism.thenApply(Integer::valueOf);
+		CompletableFuture<Double> salami = fridge.thenApply( x -> Math.PI*x*x);
+		salami.thenAccept(System.out::println);//will happen when the hell freezes over
 		
 		System.out.println("END");
 	}
