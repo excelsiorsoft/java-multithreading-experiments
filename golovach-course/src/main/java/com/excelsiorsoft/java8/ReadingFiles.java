36a95fe2dc99f25cd70b72e1058c81c331e643c1
sleyzerzon <senya22@yahoo.com>
2016-09-10 11:38:56 -0400

    futures

--- golovach-course/src/main/java/com/excelsiorsoft/java8/ReadingFiles.java
+++ golovach-course/src/main/java/com/excelsiorsoft/java8/ReadingFiles.java
@@ -1,8 +1,12 @@
 package com.excelsiorsoft.java8;
 
+import static java.util.Arrays.asList;
+
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -13,7 +17,10 @@ public class ReadingFiles {
 	ExecutorService pool = Executors.newCachedThreadPool();
 	
 	public static void main(String[]args) throws IOException, InterruptedException, ExecutionException {
-		new ReadingFiles().execute();
+		ReadingFiles readingFiles = new ReadingFiles();
+		readingFiles.execute();
+		System.out.println("-------------------------");
+		readingFiles.invokeAll();
 	}
 	
 	public void execute() throws InterruptedException, ExecutionException {
@@ -24,6 +31,26 @@ public class ReadingFiles {
 		System.out.println(futBytes0.isDone());
 		byte[] bytes = futBytes0.get();
 		System.out.println(futBytes0.isDone());
+		
+	
+	}
+	
+	public void invokeAll() throws InterruptedException, ExecutionException {
+		
+		List<Future<byte[]>> listFutBytes = pool.invokeAll(
+				asList(
+						() -> Files.readAllBytes(Paths.get("E:\\source\\java-multithreading-experiments\\golovach-course\\src\\test\\resources\\tmp0.txt")),
+						() -> Files.readAllBytes(Paths.get("E:\\source\\java-multithreading-experiments\\golovach-course\\src\\test\\resources\\tmp0.txt")),
+						() -> Files.readAllBytes(Paths.get("E:\\source\\java-multithreading-experiments\\golovach-course\\src\\test\\resources\\tmp0.txt"))
+					)
+				);
+		
+		
+		System.out.println(listFutBytes.get(0).isDone());
+		System.out.println(listFutBytes.get(1).isDone());
+		System.out.println(listFutBytes.get(2).isDone());
+		
+		
 	
 	}
 	
