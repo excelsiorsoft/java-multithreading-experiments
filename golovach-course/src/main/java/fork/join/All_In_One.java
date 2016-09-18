3f350e35a984a6e09f22922ab74aa93c5114079e
sleyzerzon <senya22@yahoo.com>
2016-09-18 12:18:03 -0400

    more effective way

--- golovach-course/src/main/java/fork/join/All_In_One.java
+++ golovach-course/src/main/java/fork/join/All_In_One.java
@@ -44,4 +44,42 @@ public class All_In_One {
 	}
 	
 	
+public void betterImplOfIterativeParallelism() throws InterruptedException {
+	
+	AtomicLong result = new AtomicLong(0);
+	int cpuCount = Runtime.getRuntime().availableProcessors();
+	ExecutorService pool = Executors.newFixedThreadPool(cpuCount);
+	
+	long start = System.nanoTime();
+	List<Callable<Void>>taskList = new ArrayList<>();
+	
+	for(int k = 0; k <100;k++) {
+		final int finalK = k;  //effectively final is allowed as well
+		taskList.add(()->{
+			//[0...10,000), [10,000...20,000), ...
+			long localResult = calcMoreEffective(/*result, */10_000 * finalK, 10_000 * (finalK + 1));
+			result.addAndGet(localResult);
+			return null;
+		});
+	}
+	
+	pool.invokeAll(taskList); //blocking call
+	long end = System.nanoTime();
+	System.out.println(result + "; "+ (end - start)/1_000_000 +" ms");
+	pool.shutdown();
+
+}
+
+private long calcMoreEffective(/*AtomicLong accum,*/ int from, int to) {
+		long result = 0;
+		for(int index = from; index < to; index++) {
+			if(index % 3 !=0 && index %5 != 0) {
+				/*accum.addAndGet(index);*/
+				result += index;
+			}
+		}
+		return result;
+	}
+	
+
 }
--- golovach-course/src/test/java/fork/join/All_In_OneTest.java
+++ golovach-course/src/test/java/fork/join/All_In_OneTest.java
@@ -9,8 +9,14 @@ public class All_In_OneTest {
 	All_In_One cut =new All_In_One();
 	
 	@Test
-	public void test() throws InterruptedException {
+	public void nonEffectiveIterativeParallelism() throws InterruptedException {
 		cut.nonEffectiveIterativeParallelism();
 	}
+	
+	@Test
+	public void betterImplOfIterativeParallelism() throws InterruptedException {
+		cut.betterImplOfIterativeParallelism();
+	}
+	
 
 }
